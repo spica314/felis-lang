6fcc7e49cf90e44bde3c0c050a6eb8d2ff825dee
Masato Mori <13274198+spica314@users.noreply.github.com>
2025-10-09 23:53:01 +0900

    Delete type checker

--- neco-bootstrap/neco-cic/README.md
+++
@@ -1,7 +0,0 @@
-# neco-cic
-
-Calculus of Inductive Constructions Kernel
-
-## References
-
-- https://rocq-prover.org/doc/V8.9.1/refman/language/cic.html
--- neco-bootstrap/neco-cic/src/lib.rs
+++
@@ -1,8 +0,0 @@
-pub mod global_environment;
-pub mod id;
-pub mod inductive;
-pub mod local_context;
-pub mod reduction;
-pub mod substitution;
-pub mod term;
-pub mod typechecker;
