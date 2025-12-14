03b3f414ccb8f74555000746c3bcaf36be10bbaf
Masato Mori <13274198+spica314@users.noreply.github.com>
2025-12-14 13:10:08 +0900

    Add neco-typing crate with term AST and replace logic
    Change-Id: I1e21ba94a77ffa5f593cff31a4a3a6926a6a6964

---
+++ stage0/neco-typing/src/lib.rs
@@ -0,0 +1,40 @@
+mod replace;
+pub use replace::replace;
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct TermVariable {
+    pub variable_id: usize,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct TermForall {
+    pub variable_id: usize,
+    pub variable_ty: Box<Term>,
+    pub body: Box<Term>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct TermApply {
+    pub f: Box<Term>,
+    pub x: Box<Term>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct TermMatch {
+    pub arms: Vec<TermMatchArm>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct TermMatchArm {
+    pub constructor: TermVariable,
+    pub constructor_args: Vec<TermVariable>,
+    pub term: Box<Term>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum Term {
+    Variable(TermVariable),
+    Forall(TermForall),
+    Apply(TermApply),
+    Match(TermMatch),
+}
---
+++ stage0/neco-typing/src/replace.rs
@@ -0,0 +1,162 @@
+use crate::*;
+
+pub fn replace(t: &Term, from: usize, to: Term) -> Term {
+    match t {
+        Term::Variable(variable) => {
+            if variable.variable_id == from {
+                to.clone()
+            } else {
+                t.clone()
+            }
+        }
+        Term::Forall(forall) => {
+            if forall.variable_id == from {
+                panic!()
+            }
+            let sub_ty = replace(&forall.variable_ty.clone(), from, to.clone());
+            let sub_term = replace(&forall.body.clone(), from, to.clone());
+            Term::Forall(TermForall {
+                variable_id: forall.variable_id,
+                variable_ty: Box::new(sub_ty),
+                body: Box::new(sub_term),
+            })
+        }
+        Term::Apply(apply) => {
+            let sub_f = replace(&apply.f.clone(), from, to.clone());
+            let sub_x = replace(&apply.x.clone(), from, to.clone());
+            Term::Apply(TermApply {
+                f: Box::new(sub_f),
+                x: Box::new(sub_x),
+            })
+        }
+        Term::Match(term_match) => {
+            let mut arms = vec![];
+            for arm in &term_match.arms {
+                if arm.constructor.variable_id == from {
+                    panic!();
+                }
+                for constructor_arg in &arm.constructor_args {
+                    if constructor_arg.variable_id == from {
+                        panic!();
+                    }
+                }
+
+                let sub_term = replace(&arm.term.clone(), from, to.clone());
+                arms.push(TermMatchArm {
+                    constructor: arm.constructor.clone(),
+                    constructor_args: arm.constructor_args.clone(),
+                    term: Box::new(sub_term),
+                })
+            }
+            Term::Match(TermMatch { arms: arms })
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn var(id: usize) -> Term {
+        Term::Variable(TermVariable { variable_id: id })
+    }
+
+    #[test]
+    fn replace_variable_matches() {
+        let t = var(1);
+        let replaced = replace(&t, 1, var(2));
+        assert_eq!(replaced, var(2));
+    }
+
+    #[test]
+    fn replace_variable_non_match() {
+        let t = var(1);
+        let replaced = replace(&t, 2, var(3));
+        assert_eq!(replaced, var(1));
+    }
+
+    #[test]
+    fn replace_under_forall_non_shadowing() {
+        let t = Term::Forall(TermForall {
+            variable_id: 10,
+            variable_ty: Box::new(var(1)),
+            body: Box::new(Term::Apply(TermApply {
+                f: Box::new(var(1)),
+                x: Box::new(var(2)),
+            })),
+        });
+        let replaced = replace(&t, 1, var(99));
+        let expected = Term::Forall(TermForall {
+            variable_id: 10,
+            variable_ty: Box::new(var(99)),
+            body: Box::new(Term::Apply(TermApply {
+                f: Box::new(var(99)),
+                x: Box::new(var(2)),
+            })),
+        });
+        assert_eq!(replaced, expected);
+    }
+
+    #[test]
+    #[should_panic]
+    fn replace_panics_when_forall_binds_from() {
+        let t = Term::Forall(TermForall {
+            variable_id: 1,
+            variable_ty: Box::new(var(2)),
+            body: Box::new(var(3)),
+        });
+        let _ = replace(&t, 1, var(99));
+    }
+
+    #[test]
+    fn replace_under_match_non_shadowing() {
+        let t = Term::Match(TermMatch {
+            arms: vec![TermMatchArm {
+                constructor: TermVariable { variable_id: 10 },
+                constructor_args: vec![TermVariable { variable_id: 20 }],
+                term: Box::new(Term::Apply(TermApply {
+                    f: Box::new(var(1)),
+                    x: Box::new(var(2)),
+                })),
+            }],
+        });
+        let replaced = replace(&t, 1, var(42));
+        let expected = Term::Match(TermMatch {
+            arms: vec![TermMatchArm {
+                constructor: TermVariable { variable_id: 10 },
+                constructor_args: vec![TermVariable { variable_id: 20 }],
+                term: Box::new(Term::Apply(TermApply {
+                    f: Box::new(var(42)),
+                    x: Box::new(var(2)),
+                })),
+            }],
+        });
+        assert_eq!(replaced, expected);
+    }
+
+    #[test]
+    #[should_panic]
+    fn replace_panics_when_match_binds_from() {
+        let t = Term::Match(TermMatch {
+            arms: vec![TermMatchArm {
+                constructor: TermVariable { variable_id: 1 },
+                constructor_args: vec![],
+                term: Box::new(var(2)),
+            }],
+        });
+        let _ = replace(&t, 1, var(99));
+    }
+
+    #[test]
+    #[should_panic]
+    fn replace_panics_when_match_constructor_arg_binds_from() {
+        let t = Term::Match(TermMatch {
+            arms: vec![TermMatchArm {
+                constructor: TermVariable { variable_id: 10 },
+                constructor_args: vec![TermVariable { variable_id: 1 }],
+                term: Box::new(var(2)),
+            }],
+        });
+        let _ = replace(&t, 1, var(99));
+    }
+}
