170b649cf1084e2314ba8c96334a21f1e2fdd5eb
Masato Mori <13274198+spica314@users.noreply.github.com>
2025-12-20 19:40:23 +0900

    Validate type defs end in a sort to reject invalid types
    Change-Id: I88707af02efb093551fb02bff5fcf1706a6a6964

--- stage0/neco-typing/src/check.rs
+++ stage0/neco-typing/src/check.rs
@@ -19,9 +19,22 @@ pub fn check(defs: &Definitions) -> bool {
 }
 
 fn check_type_def(_defs: &Definitions, _variable_id: usize, _type_def: &TypeDef) -> bool {
-    true
+    check_type_def_final_sort(_type_def)
 }
 
 fn check_variable(_defs: &Definitions, _variable_id: usize, _term_ty: &Term, _term: &Term) -> bool {
     true
+}
+
+fn check_type_def_final_sort(type_def: &TypeDef) -> bool {
+    check_term_final_sort(&type_def.ty)
+}
+
+fn check_term_final_sort(term: &Term) -> bool {
+    match term {
+        Term::Sort(_) => true,
+        Term::Arrow(arrow) => check_term_final_sort(&arrow.to),
+        Term::Forall(forall) => check_term_final_sort(&forall.body),
+        _ => false,
+    }
 }
